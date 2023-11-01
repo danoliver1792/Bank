e540cea4af009411b0cc27b4dfce23ad2b08618c
danrlei-oliveira <danrlei.jesus@hotmail.com>
2023-11-01 20:43:00 -0300

    primeira parte do banco em c sharp

---
+++ Bank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class ContaBancaria
+{
+    private double saldo;
+
+    // construtor que inicializa o saldo da conta com um valor inicial
+    public ContaBancaria(double saldoInicial)
+    {
+        saldo = saldoInicial;
+    }
+
+    // propriedade para obter o saldo atual
+    public double Saldo
+    {
+        get { return saldo; }
+    }
+
+    // método para depositar dinheiro na conta
+    public void Depositar(double valor)
+    {
+        if (valor > 0)
+        {
+            saldo += valor;
+            Console.WriteLine($"Deposito de R${valor} realizado com sucesso");
+        }
+        else
+        {
+            Console.WriteLine("Valor de deposito invalido");
+        }
+    }
+}
