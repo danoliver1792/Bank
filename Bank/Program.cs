e98d24b44c2afa4db35e71f1f7122ee89718777c
danrlei-oliveira <danrlei.jesus@hotmail.com>
2023-11-01 20:53:36 -0300

    terceira parte do banco em c sharp

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -49,6 +49,46 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Bem-vindo ao Banco");
+        Console.WriteLine("Digite o saldo inicial da conta: ");
+        double saldoInicial = double.Parse(Console.ReadLine());
 
+        // cria uma instância da classe ContaBancaria com o saldo inicial fornecido
+        ContaBancaria conta = new ContaBancaria(saldoInicial);
+
+        while (true)
+        {
+            Console.WriteLine("\nEscolha uma opcao: ");
+            Console.WriteLine("1. Verificar Saldo");
+            Console.WriteLine("2. Depositar Dinheiro");
+            Console.WriteLine("3. Sacar Dinheiro");
+            Console.WriteLine("4. Sair");
+
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine($"Saldo Atual: R${conta.Saldo}");
+                    break;
+                case 2:
+                    Console.WriteLine("Digite o valor a ser depositado");
+                    double valorDepositado = double.Parse(Console.ReadLine());
+                    conta.Depositar(valorDepositado);
+                    break;
+                case 3:
+                    Console.WriteLine("Digite o valor a ser sacado: ");
+                    double valorSaque = double.Parse(Console.ReadLine());
+                    conta.Sacar(valorSaque);
+                    break;
+                case 4:
+                    Console.WriteLine("Obrigado por usar o banco");
+                    Environment.Exit(0);
+                    break;
+                case 5:
+                    Console.WriteLine("Opcao Invalida. Tente Novamente");
+                    break;
+            }
+        }
     }
 }
